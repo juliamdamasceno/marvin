10ffa3bc15bd18dfb96428ccc6f83cac992cb578
juliamdamasceno <52722928+juliamdamasceno@users.noreply.github.com>
2019-07-11 20:41:41 -0300

    fase02/ex04

---
+++ fase02/ex04/maiusculo.js
@@ -0,0 +1,7 @@
+function maiusculo (argumento){
+    var palavra = argumento;
+    var palavra1 = palavra.substr(0,3);
+    palavra = palavra.replace(palavra1, palavra1.toUpperCase());
+    return palavra;
+}
+    console.log(maiusculo("folha"))
