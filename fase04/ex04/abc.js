95577ec134aa61941cf84d5ab385655a6f0ae09d
juliamdamasceno <52722928+juliamdamasceno@users.noreply.github.com>
2019-07-23 19:26:38 -0300

    fase04/ex04

---
+++ fase04/ex04/abc.js
@@ -0,0 +1,12 @@
+function abc (argumento){
+    var primeira = argumento.substr(0,1);
+    if (primeira == "a")
+        return ("true");
+    if (primeira == "b")
+        return ("true");
+    if (primeira == "c")
+        return ("true");
+    else {
+        return ("false");
+    }
+}
