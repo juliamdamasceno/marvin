ddf6b27e26945ac81611da9191cd7ac3ab8c4a6c
juliamdamasceno <52722928+juliamdamasceno@users.noreply.github.com>
2019-07-23 20:21:43 -0300

    fase04/ex07

---
+++ fase04/ex07/acharNumero.js
@@ -0,0 +1,10 @@
+function acharNumero (num1){
+   if (num1 < 5)
+   return ("a");
+   if (num1 >= 5 && num1 <= 10)
+   return ("b");
+   if (num1 >= 10 && num1 <= 100)
+   return ("c");
+   if (num1 > 100)
+   return ("d");
+}
