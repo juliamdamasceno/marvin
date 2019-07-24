f5079e0ce9f928b41bce3473e903d2c348487ed9
juliamdamasceno <52722928+juliamdamasceno@users.noreply.github.com>
2019-07-23 21:02:39 -0300

    fase04/ex08

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,15 @@
+function alfabeloMilitar (vogal){
+    if (vogal == "a")
+        return ("alfa");
+    if (vogal == "e")
+        return ("echo");
+    if (vogal == "i")
+        return ("india");
+    if (vogal == "o")
+        return ("oscar");
+    if (vogal == "u")
+        return ("uniform");
+    else {
+        return ("false");
+    }
+}
