94526fa976593fbe229e7de46b91e8f893ff34fb
juliamdamasceno <52722928+juliamdamasceno@users.noreply.github.com>
2019-08-01 20:46:32 -0300

    fase06/ex00

--- fase06/ex00/entreNumeros.js
+++ fase06/ex00/entreNumeros.js
@@ -1,3 +1,16 @@
-function entreNumeros (min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function entreNumeros (num1, num2){
+    var min;
+    var max;
+    if (num1<num2){
+        min = num1;
+        max = num2;
+    }
+    else{
+        min = num2;
+        max = num1;
+    }
+    while (min <= max){
+    console.log(min);
+    min++;
+    }
 }
